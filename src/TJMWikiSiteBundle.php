d80ad8abd566de079638393aa6732e351ee8dcfb
Toby Mackenzie <public@tobymackenzie.com>
2023-10-07 17:36:05 -0400

    fix: was not working as a bundle

--- src/TJMWikiSiteBundle.php
+++ src/TJMWikiSiteBundle.php
@@ -2,4 +2,9 @@
 namespace TJM\WikiSiteBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class TJMWikiSiteBundle extends Bundle{}
+class TJMWikiSiteBundle extends Bundle{
+	//-@ via <https://github.com/symfony/symfony/pull/32845/files>, should use new directory structure
+	public function getPath(): string{
+		return \dirname(__DIR__);
+	}
+}
