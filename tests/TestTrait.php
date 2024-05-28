6fc56d15713922a14195248aa8769aee6c331f43
Toby Mackenzie <public@tobymackenzie.com>
2024-05-28 15:19:22 -0400

    test: fix risky issue in kernel tests for PHPUnit 11, related to Symfony's `ErrorHandler`

--- tests/TestTrait.php
+++ tests/TestTrait.php
@@ -1,11 +1,14 @@
 <?php
 namespace TJM\WikiSite\Tests;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 trait TestTrait{
 	const WIKI_DIR = __DIR__ . '/tmp';
 
 	static public function setUpBeforeClass(): void{
 		mkdir(self::WIKI_DIR);
+		//--hack, for Symfony's exception handler in PHPUnit 11+ see <https://github.com/symfony/symfony/issues/53812#issuecomment-1958859357>
+		ErrorHandler::register(null, false);
 	}
 	protected function tearDown(): void{
 		shell_exec("rm -rf " . self::WIKI_DIR . "/.git && rm -rf " . self::WIKI_DIR . "/*");
