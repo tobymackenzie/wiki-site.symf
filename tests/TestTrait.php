bac008f4e4b6bde895a671f230c634ef3d1ad652
Toby Mackenzie <public@tobymackenzie.com>
2024-05-28 15:27:00 -0400

    test: only fix ErrorHandler notice if we need to, eg `WebTestCase`

--- tests/TestTrait.php
+++ tests/TestTrait.php
@@ -1,5 +1,6 @@
 <?php
 namespace TJM\WikiSite\Tests;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
 trait TestTrait{
@@ -8,7 +9,9 @@ trait TestTrait{
 	static public function setUpBeforeClass(): void{
 		mkdir(self::WIKI_DIR);
 		//--hack, for Symfony's exception handler in PHPUnit 11+ see <https://github.com/symfony/symfony/issues/53812#issuecomment-1958859357>
-		ErrorHandler::register(null, false);
+		if(is_subclass_of(static::class, WebTestCase::class)){
+			ErrorHandler::register(null, false);
+		}
 	}
 	protected function tearDown(): void{
 		shell_exec("rm -rf " . self::WIKI_DIR . "/.git && rm -rf " . self::WIKI_DIR . "/*");
