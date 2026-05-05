1eb2990d79ee27b6c5c09187e5a4a5439d2d54e1
Toby Mackenzie <public@tobymackenzie.com>
2026-05-04 20:35:06 -0400

    fix: path passed to template should be absolute for textish responses

--- src/WikiSite.php
+++ src/WikiSite.php
@@ -186,7 +186,7 @@ class WikiSite{
 				$adat->setData([
 					'format'=> $adat->getExtension(),
 					'name'=> $adat->getName(),
-					'canonical'=> $this->router ? $this->router->generate($this->viewRoute, ['path'=> $adat->getCanonical()], RouterInterface::ABSOLUTE_PATH) : $adat->getCanonical(),
+					'canonical'=> $this->router ? $this->router->generate($this->viewRoute, ['path'=> $adat->getCanonical()], $isHtmlish ? RouterInterface::ABSOLUTE_PATH : RouterInterface::ABSOLUTE_URL) : $adat->getCanonical(),
 					'content'=> $adat->getContent(),
 					'pagePath'=> substr($adat->getPagePath(), 1),
 					'shellTemplate'=> $this->getTemplateForExtension($this->shellTemplate, $adat->getExtension()),
